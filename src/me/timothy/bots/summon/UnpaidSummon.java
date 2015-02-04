52c56806641fcce33d4091b5fa06fbe8f7ca1d2a
Tjstretchalot <mtimothy983@gmail.com>
2015-02-04 06:25:47 -0800

    Change unpaid summon to new format; thats the last of them!

--- src/me/timothy/bots/summon/UnpaidSummon.java
+++ src/me/timothy/bots/summon/UnpaidSummon.java
@@ -5,13 +5,15 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import me.timothy.bots.BotUtils;
 import me.timothy.bots.Database;
 import me.timothy.bots.FileConfiguration;
 import me.timothy.bots.LoansBotUtils;
 import me.timothy.bots.LoansDatabase;
 import me.timothy.bots.models.Loan;
 import me.timothy.bots.models.User;
+import me.timothy.bots.responses.ResponseFormatter;
+import me.timothy.bots.responses.ResponseInfo;
+import me.timothy.bots.responses.ResponseInfoFactory;
 import me.timothy.jreddit.info.Comment;
 
 import org.apache.logging.log4j.Level;
@@ -32,6 +34,7 @@ public class UnpaidSummon implements CommentSummon {
 	 * $unpaid /u/Asdf_Jkl
 	 */
 	private static final Pattern UNPAID_PATTERN = Pattern.compile("\\s*\\$unpaid\\s/u/\\S+");
+	private static final String UNPAID_FORMAT = "$unpaid <user1>";
 
 	private Logger logger;
 
@@ -46,35 +49,26 @@ public class UnpaidSummon implements CommentSummon {
 		
 		if(matcher.find()) {
 			String group = matcher.group().trim();
-			String[] split = group.split("\\s");
-			
-			String doer = comment.author();
-			String doneTo = BotUtils.getUser(split[1]);
-			
-
+			ResponseInfo responseInfo = ResponseInfoFactory.getResponseInfo(UNPAID_FORMAT, group);
 			LoansDatabase database = (LoansDatabase) db;
-			if(config.getList("banned").contains(doneTo.toLowerCase())) {
-				logger.info("Someone is attempting to $unpaid a banned user");
-				return new SummonResponse(SummonResponse.ResponseType.INVALID, config.getString("action_to_banned"));
-			}
+
+			User author = database.getUserByUsername(responseInfo.getObject("author").toString());
+			User user1U = database.getUserByUsername(responseInfo.getObject("user1").toString());
 			
-			User doneToU = database.getUserByUsername(doneTo);
-			User doerU = database.getUserByUsername(doer);
-
-			List<Loan> relevantLoans = (doneToU != null && doerU != null) ? database.getLoansWithBorrowerAndOrLender(doneToU.id, doerU.id, true) : new ArrayList<Loan>();
+			List<Loan> relevantLoans = (author != null && user1U != null) ? database.getLoansWithBorrowerAndOrLender(user1U.id, author.id, true) : new ArrayList<Loan>();
 			List<Loan> changed = new ArrayList<>();
-
+			
 			for(Loan l : relevantLoans) {
 				if(l.principalRepaymentCents != l.principalCents) {
 					database.setLoanUnpaid(l, true);
 					changed.add(l);
 				}
 			}
-
-			logger.printf(Level.INFO, "%s has defaulted on %d loans from %s", doneTo, changed.size(), doer);
-
-			String response = config.getString("unpaid").replace("<lender>", doer).replace("<borrower>", doneTo).replace("<loans>", LoansBotUtils.getLoansAsTable(changed, database, changed.size()));
-			return new SummonResponse(SummonResponse.ResponseType.VALID, response);
+			responseInfo.addTemporaryString("changed loans", LoansBotUtils.getLoansAsTable(changed, database, changed.size()));
+			logger.printf(Level.INFO, "%s has defaulted on %d loans from %s", user1U.username, changed.size(), author.username);
+			
+			String responseFormat = config.getString("unpaid");
+			return new SummonResponse(SummonResponse.ResponseType.VALID, new ResponseFormatter(responseFormat, responseInfo).getFormattedResponse(config, database));
 		}
 		return null;
 	}
